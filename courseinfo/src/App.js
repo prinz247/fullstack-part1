8a86e60b6ba31aa96120b7b8b70fc52b9c8399d2
Prince Owusu <prinze247@gmail.com>
2023-03-09 10:28:48 +0200

    step 2

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -8,19 +8,23 @@ const Header = (props) =>{
   )
 }
 
-{/*const Part = (props) =>{
-  return(
-    
-  )
-}*/}
-
-const Content = (props) => {
-  console.log(props)
+const Part = (props) =>{
   return(
     <div>
       <p>{props.part1} {props.exercises1}</p>
       <p>{props.part2} {props.exercises2}</p>
       <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Part part1={props.part1} exercises1= {props.exercises1} />
+      <Part part2={props.part2} exercises2= {props.exercises2} />
+      <Part part3={props.part3} exercises3= {props.exercises3} />
     </div>
   )
 }
